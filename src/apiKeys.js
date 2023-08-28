8a631b06c3e1d22299d773e15664756c12a2c460
TahirRehman1995 <49286139+TahirRehman1995@users.noreply.github.com>
2023-08-29 00:57:53 +0500

    final project submitted

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "cd66680025fe5be4e861b62d3979409a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
